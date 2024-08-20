af77cc6d8bab5756c45edd136c803b547c67ed34
Elaine Han <62453103+elainehan3@users.noreply.github.com>
2024-08-20 18:49:55 +0000

    test

--- main.py
+++ main.py
@@ -95,9 +95,9 @@ def submit_handler(event = None):
     if event:
         event.preventDefault()
         output_div = document.querySelector("#output")
-        output_div.innerText = os.listdir('/') 
+        output_div.innerText = os.listdir('/') + os.listdir('./')
         t,r,b,l,a = get_input()
-        #solutions = get_solutions(t,r,b,l,a)
+        solutions = get_solutions(t,r,b,l,a)
         output_div = document.querySelector("#output")
         output_div.innerText = os.listdir('/') 
 
