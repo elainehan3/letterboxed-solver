d8323e82d4b645c504d902f625ac36036ce719cd
Elaine Han <62453103+elainehan3@users.noreply.github.com>
2024-08-27 00:34:52 +0000

    v1

--- main.py
+++ main.py
@@ -59,46 +59,57 @@ def get_words(sides, t: str, r: str, b: str, l: str):
         # valid = [word for word in file]
         # return set(valid)
 
-def one_word_solution(word_list, chars):
-    return [w for w in word_list if set(w) == chars]
+def one_word_sols(words, allowed):
+    output = ["<p>" + word + "</p>" for word in words if set(word) == allowed]
+    if len(output) == 0:
+        output = ["NO ONE WORD SOLUTIONS FOUND"]
+    return "".join(output)
 
 # find two word solutions
-def two_word_solution(word_list, chars):
+def two_word_sols(words, allowed):
     output = []
-    for word in word_list:
+    for word in words:
         last = word[len(word)-1]
-        matches = [w for w in word_list if w[0] == last and w!= word]
-        for m in matches:
-            pair = word + m
-            if set(pair) == chars:
-                output.append([word,m])
-    return output
+        matches = [w for w in words if w[0] == last and w!= word]
+        for match in matches:
+            pair = word + match
+            if set(pair) == allowed:
+                output.append("<p>" + word + "→" + match + "</p>")
+    return "".join(output)
 
 # find three word solutions
-def three_word_solution(word_list, chars):
-    ab = [a+b for a in word_list for b in word_list if a[-1]==b[0]]
+def three_word_solutions(words, chars):
+    ab = [a+b for a in words for b in words if a[-1]==b[0]]
     candidates = list(set([to_base(a)+a[-1] for a in ab]))
-    solutions = {a:b for a in candidates for b in word_list if set(a+b)==chars and a[-1]==b[0]}
-    ext = [[a+'-'+b,to_base(a+b)+b[-1]] for a in word_list for b in word_list if a!=b and a[-1]==b[0]]
+    solutions = {a:b for a in candidates for b in words if set(a+b)==chars and a[-1]==b[0]}
+    ext = [[a+'-'+b,to_base(a+b)+b[-1]] for a in words for b in words if a!=b and a[-1]==b[0]]
     vals = ['-'.join([e[0],solutions[e[1]]]) for e in ext if e[1] in solutions.keys()]
     return [v.split('-') for v in vals]
 
 def get_solutions(t: str, r: str, b: str, l: str, already:str):
     sides = get_dict(t,r,b,l)
     valid_words = get_words(sides, t, r, b, l)
-    return valid_words
+    allowed = set(t+r+b+l)
+    one_word_solution = one_word_sols(valid_words, allowed)
+    two_word_solution = two_word_sols(valid_words, allowed)
+    return one_word_solution, two_word_solution
 
-def display_solutions():
-    output_div = document.querySelector("#output")
-    output_div.innerText = english
+def display_solutions(one_word, two_word):
+    head_one = document.querySelector("#one_word_head")
+    head_two = document.querySelector("#two_word_head")
+    div_one = document.querySelector("#one_word_sols")
+    div_two = document.querySelector("#two_word_sols")
+    head_one.innerHTML = "<h2>One Word Solutions</h2>"
+    head_two.innerHTML = "<h2>Two Word Solutions</h2>"
+    div_one.innerHTML = one_word
+    div_two.innerHTML = two_word
 
 def submit_handler(event = None):
     if event:
         event.preventDefault()
         t,r,b,l,a = get_input()
-        solutions = get_solutions(t,r,b,l,a)
-        output_div = document.querySelector("#output")
-        output_div.innerText = solutions 
+        one_word_solution, two_word_solution = get_solutions(t,r,b,l,a)
+        display_solutions(one_word_solution, two_word_solution)
 
 
 def change_input(t, r, b, l, a): # unused
